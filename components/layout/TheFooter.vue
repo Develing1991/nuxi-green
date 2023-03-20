b26413069f7c7037185a10caaaea7afd791c4ea1
Develing1991 <completed0728@gmail.com>
2023-03-21 08:05:00 +0900

    footer

--- components/layout/TheFooter.vue
+++ components/layout/TheFooter.vue
@@ -1,8 +1,36 @@
 <script lang="ts" setup></script>
 
 <template>
-  <footer class="max-w-3xl mx-auto ">
-    footer
+  <footer class="max-w-3xl mx-auto text-sm text-white bg-zinc-900  mt-16 p-4 leading-6">
+    <ul class="flex gap-4 max-[480px]:flex-col ">
+      <li>
+        <button>서비스 이용약관</button>
+      </li>
+      <li>
+        <button>개인정보 처리방침</button>
+      </li>
+      <li>
+        <button>입점신청</button>
+      </li>
+      <li>
+        <button>제휴점 관리자 페이지</button>
+      </li>
+    </ul>
+    <p class="flex gap-x-4 flex-wrap mt-4 max-[480px]:flex-col">
+      <span class="font-bold">(주) DEV COMPANY</span>
+      <span>사업자번호: 000-00-00000</span>
+      <span>대표이사: LEE SU HAN</span>
+      <span>통신판매업: 서울강남-0000</span>
+      <span>이메일: completed0728@gmail.com</span>
+    </p>
+    <address class="flex gap-x-4 flex-wrap mt-4 not-italic ">
+      <span>주소: 서울시 강남구 마음대로 111, 000호(링딩동, 슈퍼벨 2차)</span>
+      <span>고객센터: 1600-0000</span>
+      <span>운영시간: 평일 오전 9시~오후 5시 30분 / 주말,공휴일 휴무 (점심시간 오후 12시~오후 1시)</span>
+    </address>
+    <p class="mt-4">
+      ⓒ 2019 DEVELING COMPANY & 1991 INC. All Rights Reserved.
+    </p>
   </footer>
 </template>
 

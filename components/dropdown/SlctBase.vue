45fafeffd5cbb4fe051a029869478010f0032220
Develing1991 <completed0728@gmail.com>
2023-03-21 14:43:23 +0900

    dropdown custom

---
+++ components/dropdown/SlctBase.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const arr = ref([
+  '선택',
+  '낮은 가격순',
+  '판매량순',
+  '리뷰많은순',
+  '높은 가격순',
+  '최신순'])
+const show = ref(false)
+const selected = ref('선택')
+const onSelectToggle = (item: string) => {
+  show.value = !show.value
+  selected.value = item
+}
+</script>
+
+<template>
+  <div aria-label="정렬 선택" class="relative">
+    <button class="w-32 border rounded py-1 border-gray-800" @click="onSelectToggle('선택')">
+      {{ selected }}
+    </button>
+    <ul v-show="show" class="w-32 border rounded border-gray-800 flex flex-col items-center justify-center absolute top-0 left-0 ">
+      <li v-for="(item, index) in arr" :key="index" class="py-1 border-b w-full text-center cursor-pointer bg-white hover:bg-cyan-300" @click="onSelectToggle(item)">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -12,13 +12,7 @@ const gridToggle = (col: number) => {
   <TheMain>
     <section>
       <!-- dropdown select -->
-      <ul class="" aria-label="정렬 선택">
-        <li>낮은 가격순</li>
-        <li>판매량순</li>
-        <li>리뷰많은순</li>
-        <li>높은 가격순</li>
-        <li>최신순</li>
-      </ul>
+      <SlctBase />
       <div class="hidden  max-[480px]:block">
         <Icon v-show="cols === 1" name="ic:sharp-grid-view" size="25" class="cursor-pointer" @click="gridToggle(2)" />
         <Icon v-show="cols === 2" name="ic:sharp-view-list" size="30" class="cursor-pointer" @click="gridToggle(1)" />
